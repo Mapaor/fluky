8c7d403361a7cb97b1d76f2ad059ae5bc52156a1
Martí <mpardoor24@alumnes.ub.edu>
2025-07-07 14:49:19 +0200

    Update script.js

--- script.js
+++ script.js
@@ -150,7 +150,7 @@
 
       // Responsive font and placement
       let isMobile = window.innerWidth <= 600;
-      let fontSize = isMobile ? Math.round(canvas.width / 10) : 140;
+      let fontSize = isMobile ? Math.round(canvas.width / 8) : 140;
       ctx.font = fontSize + "px Georgia";
       ctx.fillStyle = 'white';
       // Calculate text widths for recentring
